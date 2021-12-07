a8568db74d2dae2f5bad901843a181cad3cea9ed
GurAnttal <anttal.guru@gmail.com>
2021-12-07 21:49:02 +0530

    Crud task

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        $users  = Auth::user();
+        $role = $users->role;
+        if(Auth::user())
+        {
+            $a = 1;
+            $user = User::where('role', 'user')->get();
+            return view('home',['a'=>$a, 'users'=>$user, 'role'=>$role]);
+        }
+    }
+
+    public function adduser(Request $request, $id=NULL)
+    {
+        if(empty($request->all()))
+        {
+            $user = User::where('role', 'user')->where('id',$id)->first();
+            return view('adduser',['id'=>$id, 'user'=>$user]);
+        }
+        else
+        {
+            $validator = Validator::make($request->all(), [     
+                'firstname' => ['required', 'string', 'max:255'],
+                'lastname' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]); 
+            if($validator->fails())
+            {
+                   // $errormessage = $validator->errors();
+                 return redirect('add-user')
+                  ->withErrors($validator)
+                  ->withInput();
+            }
+            else
+            {
+                if($id>0)
+                {
+                    $firstname = $request->input('firstname');
+                    $lastname = $request->input('lastname');
+                    $email =$request->input('email');
+                    $password = Hash::make($request->input('password'));
+                    $users = User::find($id);               
+                    $users->firstname = $firstname;
+                    $users->lastname = $lastname;
+                    $users->email = $email;
+                    $users->password = $password;
+                    $users->save();   
+                    if($users)
+                    {
+                        return redirect('home');
+                    }
+                }
+                else{
+                    $firstname = $request->input('firstname');
+                    $lastname = $request->input('lastname');
+                    $email =$request->input('email');
+                    $password = Hash::make($request->input('password'));
+                    $users = new User;               
+                    $users->firstname = $firstname;
+                    $users->lastname = $lastname;
+                    $users->email = $email;
+                    $users->password = $password;
+                    $users->role ='user';
+                    $users->save();   
+                    if($users)
+                    {
+                        return redirect('home');
+                    }
+                }
+            }
+        }
+    }
+
+
+    public function uploadfile(Request $request, $id)
+    {
+        $request->validate([
+            "file" => "required"
+        ]);
+
+        if ($request->hasFile('file')) 
+        {
+            $removeimg = User::where('id',$id)->first();
+            if ($removeimg->profile_image != '') {
+               $image_path = public_path().'/storage/file/'.$removeimg->profile_image;
+                unlink($image_path);
+            }
+            
+
+            $file = $request->file('file');
+            $name = time().'.'.$file->getClientOriginalExtension();
+            $destinationPath = public_path('/storage/file');
+            $file->move($destinationPath, $name);
+
+            $addfile = User::find($id);
+            $addfile->profile_image = $name;
+            $addfile->save(); 
+        }
+       
+        return response()->json('File uploaded successfully');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,131 @@
+@extends('layouts.app')
+
+@section('content')
+
+                    
+
+ @if($role == 'admin')              
+<div class="container table-responsive py-5"> 
+<div class="text-right">
+    <a href="{{url('/')}}/add-user" class="btn btn-primary">add user</a>
+</div>
+<br>
+<table class="table table-bordered table-hover">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Firstname</th>
+      <th scope="col">Lastname</th>
+      <th scope="col">Email</th>
+      <th scope="col">Created At</th>
+        <th scope="col">Profile Image</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  @if(count($users) > 0)
+      
+        @foreach($users as $value)
+      <tr>
+        <td>{{$a++}}</td>
+        <td>{{$value->firstname}}</td>
+       <td>{{$value->lastname}}</td>
+        <td>{{$value->email}}</td>
+         <td>{{$value->created_at}}</td>
+         <td>
+             @if($value->profile_image != '')
+                <img src="{{ asset('storage/file/' . $value->profile_image) }}" style="width:50px" />
+             
+                <form action="" id="contactform" method="post">
+                @csrf
+                    <div class="form-group">
+                        <input id="uploadfilepdf" type="file" name="sortpic" />
+                    </div>
+                    
+                    <div class="form-group">
+                      <button type="button" class="btn btn-primary" onclick="return uploadfile('{{$value->id}}')">update Now</button>
+                    </div>
+                </form>
+            @else
+                <form action="" id="contactform" method="post">
+                @csrf
+                    <div class="form-group">
+                        <input id="uploadfilepdf" type="file" name="sortpic" />
+                    </div>
+                    
+                    <div class="form-group">
+                      <button type="button" class="btn btn-primary" onclick="return uploadfile('{{$value->id}}')">Send Now</button>
+                    </div>
+                </form>
+            @endif    
+         </td>
+         <td><a href="{{url('/')}}/update-user/{{$value->id}}" class="btn btn-primary">update user</a></td>
+        
+      </tr>
+      @endforeach
+    @endif
+    
+  </tbody>
+</table>
+</div>
+@else
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    function uploadfile(userid)
+    {
+        var file_data = $('#uploadfilepdf').prop('files')[0];   
+        var form_data = new FormData();                  
+        form_data.append('file', file_data);
+        // console.log(form_data);
+        //     alert(form_data);
+
+        $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+        });
+
+        $.ajax({
+            type:'POST',
+            url: "{{ url('uploadfile') }}/"+userid,
+            data: form_data,
+            contentType: false,
+            processData: false,
+            success: (response) => {
+
+                if (response) {
+                    alert('File has been uploaded successfully');
+                    location.reload();
+                }
+            },
+            error: function(response){
+                console.log(response);
+                $('#image-input-error').text(response.responseJSON.errors.file);
+            }
+        });
+    }
+</script>
+
+
+@endsection
